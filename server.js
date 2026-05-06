7457a13705a3e95b6cc6221ca061611d2e979e0e
bartsilvera12-gif <bartsilvera12-gif@users.noreply.github.com>
2026-05-06 16:42:24 -0300

    Renombrar DI Natale.html a index.html para Vercel

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname)));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "DI Natale.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(PORT, () => {
